1651c9c769881de9766a4f0e5afb6bb6d95806c6
yuvraj-kalsi0001 <97918056+yuvraj-kalsi0001@users.noreply.github.com>
2023-09-13 16:15:30 -0230

    final

--- SNAKE_GAME/GamePanel.java
+++ SNAKE_GAME/GamePanel.java
@@ -1,15 +1,30 @@
 package SNAKE_GAME;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Random;
+
+import javax.swing.Timer;
+
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements ActionListener, KeyListener {
 
     private int[] snakexlength = new int[750];
     private int[] snakeylength = new int[750];
 
+    private int[] xPos = { 25, 50, 75, 100, 125, 150, 175, 200, 225, 250, 275, 300, 325, 350, 375, 400, 425, 450, 475,
+            500, 525, 550, 575, 600, 625, 650, 675, 700, 725, 750, 775, 800, 825, 850 };
+    private int[] yPos = { 75, 100, 125, 150, 175, 200, 225, 250, 275, 300, 325, 350, 375, 400, 425, 450, 475, 500, 525,
+            550, 575, 600, 625 };
+    private Random random = new Random();
+    private int enemyX, enemyY;
     private boolean left = false;
     private boolean right = true;
     private boolean up = false;
@@ -17,6 +32,8 @@ public class GamePanel extends JPanel {
 
     private int moves = 0;
     private int lengthOfSnake = 3;
+    private int score;
+    private boolean gameOver = false;
 
     private ImageIcon snakeTitle = new ImageIcon(getClass().getResource("snaketitle.jpg"));
     private ImageIcon leftmouth = new ImageIcon(getClass().getResource("leftmouth.png"));
@@ -24,9 +41,31 @@ public class GamePanel extends JPanel {
     private ImageIcon upmouth = new ImageIcon(getClass().getResource("upmouth.png"));
     private ImageIcon snakeimage = new ImageIcon(getClass().getResource("snakeimage.png"));
     private ImageIcon downmouth = new ImageIcon(getClass().getResource("downmouth.png"));
+    private ImageIcon enemy = new ImageIcon(getClass().getResource("enemy.png"));
+
+    private Timer timer;
+    private int delay = 100;
 
     GamePanel() {
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(true);
 
+        timer = new Timer(delay, this);
+        timer.start();
+
+        newEnemy();
+    }
+
+    private void newEnemy() {
+        enemyX = xPos[random.nextInt(34)];
+        enemyY = yPos[random.nextInt(23)];
+
+        for (int i = lengthOfSnake - 1; i > 0; i--) {
+            if (snakexlength[i] == enemyX && snakeylength[i] == enemyY) {
+                newEnemy();
+            }
+        }
     }
 
     @Override
@@ -59,7 +98,7 @@ public class GamePanel extends JPanel {
             snakeylength[0] = 100;
             snakeylength[1] = 100;
             snakeylength[2] = 100;
-            moves++;
+
         }
 
         if (left) {
@@ -79,6 +118,146 @@ public class GamePanel extends JPanel {
             snakeimage.paintIcon(this, g, snakexlength[i], snakeylength[i]);
         }
 
+        enemy.paintIcon(this, g, enemyX, enemyY);
+
+        if (gameOver) {
+            g.setColor(Color.white);
+            g.setFont(new Font("Arial", Font.BOLD, 50));
+            g.drawString("Game Over", 300, 300);
+
+            g.setFont(new Font("Arial", Font.PLAIN, 20));
+            g.drawString("Press SPACE to restart", 320, 350);
+        }
+
+        g.setColor(Color.WHITE);
+        g.setFont(new Font("Arial", Font.PLAIN, 14));
+        g.drawString("Score : " + score, 750, 30);
+        g.drawString("Length : " + lengthOfSnake, 750, 50);
+
+        g.dispose();
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+
+        for (int i = lengthOfSnake - 1; i > 0; i--) {
+            snakexlength[i] = snakexlength[i - 1];
+            snakeylength[i] = snakeylength[i - 1];
+        }
+        if (left) {
+            snakexlength[0] = snakexlength[0] - 25;
+        }
+        if (right) {
+            snakexlength[0] = snakexlength[0] + 25;
+        }
+
+        if (up) {
+            snakeylength[0] = snakeylength[0] - 25;
+        }
+        if (down) {
+            snakeylength[0] = snakeylength[0] + 25;
+        }
+
+        if (snakexlength[0] > 850)
+            snakexlength[0] = 25;
+        if (snakexlength[0] < 25)
+            snakexlength[0] = 850;
+
+        if (snakeylength[0] > 625)
+            snakeylength[0] = 75;
+        if (snakeylength[0] < 75)
+            snakeylength[0] = 625;
+
+        colldidesWithEnemy();
+        collidesWithBody();
+        repaint();
+    }
+
+    private void collidesWithBody() {
+        for (int i = lengthOfSnake - 1; i > 0; i--) {
+            if (snakexlength[i] == snakexlength[0] && snakeylength[i] == snakeylength[0]) {
+                timer.stop();
+                gameOver = true;
+            }
+        }
+    }
+
+    private void colldidesWithEnemy() {
+        if (snakexlength[0] == enemyX && snakeylength[0] == enemyY) {
+            newEnemy();
+            lengthOfSnake++;
+            score++;
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+        if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+            restart();
+        }
+
+        if (e.getKeyCode() == KeyEvent.VK_LEFT && (!right)) {
+            left = true;
+            right = false;
+            up = false;
+            down = false;
+            moves++;
+
+        }
+
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT && (!left)) {
+            left = false;
+            right = true;
+            up = false;
+            down = false;
+            moves++;
+
+        }
+
+        if (e.getKeyCode() == KeyEvent.VK_UP && (!down)) {
+            left = false;
+            right = false;
+            up = true;
+            down = false;
+            moves++;
+
+        }
+
+        if (e.getKeyCode() == KeyEvent.VK_DOWN && (!up)) {
+            left = false;
+            right = false;
+            up = false;
+            down = true;
+            moves++;
+
+        }
+    }
+
+    private void restart() {
+        gameOver = false;
+        moves = 0;
+        score = 0;
+        lengthOfSnake = 3;
+        left = false;
+        right = true;
+        up = false;
+        down = false;
+        timer.start();
+        repaint();
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
     }
 
 }
